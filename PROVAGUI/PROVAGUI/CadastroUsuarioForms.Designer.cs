4609aac85edd832a54bf9d80f67c8bdfb10ab334
Hugo Serafim <hugoluigi123@gmail.com>
2025-06-12 16:19:55 -0300

    layout cadastrousuario

--- PROVAGUI/PROVAGUI/CadastroUsuarioForms.Designer.cs
+++ PROVAGUI/PROVAGUI/CadastroUsuarioForms.Designer.cs
@@ -28,18 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            btnSalvar = new Button();
+            btnExcluir = new Button();
+            lstUsuarios = new ListBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(185, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 20);
+            label1.TabIndex = 0;
+            label1.Text = "USUARIO:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(185, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 20);
+            label2.TabIndex = 1;
+            label2.Text = "SENHA";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(185, 119);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(125, 27);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(185, 191);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(125, 27);
+            txtSenha.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(185, 233);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(94, 29);
+            btnSalvar.TabIndex = 4;
+            btnSalvar.Text = "SALVAR";
+            btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(185, 286);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(94, 29);
+            btnExcluir.TabIndex = 5;
+            btnExcluir.Text = "EXCLUIR";
+            btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // lstUsuarios
+            // 
+            lstUsuarios.FormattingEnabled = true;
+            lstUsuarios.Location = new Point(622, 65);
+            lstUsuarios.Name = "lstUsuarios";
+            lstUsuarios.Size = new Size(112, 84);
+            lstUsuarios.TabIndex = 6;
             // 
             // CadastroUsuarioForms
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lstUsuarios);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnSalvar);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "CadastroUsuarioForms";
             Text = "CadastroUsuarioForms";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Button btnSalvar;
+        private Button btnExcluir;
+        private ListBox lstUsuarios;
     }
 }
